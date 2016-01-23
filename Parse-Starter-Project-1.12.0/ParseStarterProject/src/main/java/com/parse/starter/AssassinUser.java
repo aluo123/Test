0f48e9b5d55a5cbca84b18249bd63cf2c3b8e288
Austin Luo <austin.luo06@gmail.com>
2016-01-23 02:19:58 -0500

    Create AssassinUser.java

---
+++ Parse-Starter-Project-1.12.0/ParseStarterProject/src/main/java/com/parse/starter/AssassinUser.java
@@ -0,0 +1,26 @@
+package com.parse.anywall;
+
+import com.parse.ParseClassName;
+import com.parse.ParseGeoPoint;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+/**
+ * Data model for a post.
+ */
+@ParseClassName("User")
+public class AssassinUser extends ParseUser {
+
+  public ParseGeoPoint getLocation() {
+    return getParseGeoPoint("location");
+  }
+
+  public void setLocation(ParseGeoPoint value) {
+    put("location", value);
+  }
+
+  public static ParseQuery<AnywallPost> getQuery() {
+    return ParseQuery.getQuery(AnywallPost.class);
+  }
+}
